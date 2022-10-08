299330492b7dcadd6b801ad623695b02de1538f9
gabriel <gabrielfernandes254@gmail.com>
2022-10-08 16:03:57 -0300

    refeita a parte de inclusao de tarefas e feito as funcoes de marcar tarefa feita e exluir as tarefas

--- index.js
+++ index.js
@@ -2,49 +2,57 @@ const input = document.querySelector("#input");
 const form = document.querySelector("form");
 const checkbox = document.querySelector(".checkbox");
 const listTodos = document.querySelector("#listTodos");
-
 const message = document.querySelector(".message");
 const textTodo = document.querySelector(".text-todo");
 
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
   addNewTask();
 
   input.value = "";
 });
 
+// makes the task appear on the screen when "add new task" is pressed
 function addNewTask() {
   if (input.value == "") {
     message.style.display = "block";
   } else {
-    listTodos.insertAdjacentHTML(
-      "afterbegin",
-      `
-        <ul class="todo">
-            <div>
-                <p>${input.value}</p>
-            </div>
-            <div>
-                <input class="checkbox" type="checkbox" />
-                <img class="trash-can" src="img/trash.png" alt="trash-can" />
-            </div>
-        </ul>
+    const divTodo = document.createElement("div");
+    divTodo.classList.add("todo");
+    form.appendChild(divTodo);
 
-        `
-    );
+    const todoText = document.createElement("h2");
+    todoText.innerText = input.value;
+    todoText.classList.add("text-todo");
+    divTodo.appendChild(todoText);
+
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.classList.add("checkbox");
+    divTodo.appendChild(checkbox);
+
+    const deleteIcon = document.createElement("i");
+    deleteIcon.innerHTML = '<i class="fa-solid fa-trash deleteBtn"></i>';
+    divTodo.appendChild(deleteIcon);
+
+    checkbox.addEventListener("click", () => {
+      if (checkbox.checked) {
+        todoText.style.textDecoration = "line-through";
+        todoText.style.textDecorationColor = "red";
+      } else {
+        todoText.style.textDecoration = "none";
+      }
+    });
 
     message.style.display = "none";
   }
 }
 
-function isChecked() {
-  checkbox.addEventListener("click", () => {
-    if (checkbox.checked == true) {
-      textTodo.style.textDecoration = "line-through";
-      textTodo.style.textDecorationColor = "red";
-    } else {
-      textTodo.style.textDecoration = "none";
-    }
-  });
-}
-isChecked();
+document.addEventListener("click", (e) => {
+  const targetElement = e.target;
+  const parentElement = targetElement.closest("div");
+
+  if (targetElement.classList.contains("deleteBtn")) {
+    parentElement.remove();
+  }
+});
